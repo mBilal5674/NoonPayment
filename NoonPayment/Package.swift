3411ccaa728f164aa1bad25966cc58e2aee40816
Muhammad Bilal <muhammadbilal@muhammads-mbp.home>
2024-05-20 18:42:28 +0500

    Integrated demo with swift package framework

---
+++ NoonPayment/Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "NoonPayment",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "NoonPayment",
+            targets: ["NoonPaymentSDK"]),
+    ],
+    targets: [
+        .binaryTarget(name: "NoonPaymentSDK", url: "https://docs-stg.noonpayments.com/assets/integration/NoonPaymentSDK.xcframework.zip?v=20.05.2024", checksum: "b3d709c2ca5678e65110d03f435b570a980261647af5047ada37b938ca45c22e")
+        ]
+)
