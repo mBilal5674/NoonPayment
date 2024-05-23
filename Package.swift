f948f5ffc451dfd285b37c8df75f7b091f410a99
Muhammad Bilal <muhammadbilal@muhammads-mbp.home>
2024-05-23 14:37:39 +0500

    Add Package

--- Package.swift
+++ Package.swift
@@ -11,7 +11,13 @@ let package = Package(
             name: "NoonPaymentSDK",
             targets: ["NoonPaymentSDK"]),
     ],
+//    targets: [
+//        .binaryTarget(name: "NoonPaymentSDK", url: "https://docs-stg.noonpayments.com/assets/integration/NoonPaymentSDK.xcframework.zip?v=20.05.2024", checksum: "b3d709c2ca5678e65110d03f435b570a980261647af5047ada37b938ca45c22e")
+//        ]
     targets: [
-        .binaryTarget(name: "NoonPaymentSDK", url: "https://docs-stg.noonpayments.com/assets/integration/NoonPaymentSDK.xcframework.zip?v=20.05.2024", checksum: "b3d709c2ca5678e65110d03f435b570a980261647af5047ada37b938ca45c22e")
-        ]
+            .target(
+        name: "NoonPaymentSDK",
+              dependencies:[])
+              
+    ]
 )
---
+++ Sources/NoonPaymentSDK.xcframework/ios-arm64/NoonPaymentsSDK.framework/Headers/NoonPaymentsSDK.h
@@ -0,0 +1,18 @@
+//
+//  NoonPaymentsSDK.h
+//  NoonPaymentsSDK
+//
+//  Created by Apple on 14/09/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NoonPaymentsSDK.
+FOUNDATION_EXPORT double NoonPaymentsSDKVersionNumber;
+
+//! Project version string for NoonPaymentsSDK.
+FOUNDATION_EXPORT const unsigned char NoonPaymentsSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NoonPaymentsSDK/PublicHeader.h>
+
+
